4fcbc6843cbd15a842c11fccea437d351a2138a0
Onur <sm.onurceri@gmail.com>
2024-02-20 21:14:23 +0300

    Add .env.example file and update app_config to optional .env file

--- config/app_config.py
+++ config/app_config.py
@@ -1,14 +1,14 @@
 from starlette.config import Config
+from pathlib import Path
 
-config = Config(".env")
+# Check if .env file exists, otherwise load configuration from environment variables
+env_path = ".env"
+config = Config(env_path) if Path(env_path).exists() else Config()
 
-HOSTS = config(
-    "HOSTS",
-    cast=lambda v: [i.strip() for i in v.split(",")],
-)
+HOSTS = config("HOSTS", cast=lambda v: [i.strip() for i in v.split(",")], default="")
 
-CELERY_BROKER_URL = config("CELERY_BROKER_URL", cast=str)
-CELERY_RESULT_BACKEND = config("CELERY_RESULT_BACKEND", cast=str)
+CELERY_BROKER_URL = config("CELERY_BROKER_URL", cast=str, default="")
+CELERY_RESULT_BACKEND = config("CELERY_RESULT_BACKEND", cast=str, default="")
 
 CELERY_DEFAULT_RETRY_DELAY = config("CELERY_DEFAULT_RETRY_DELAY", cast=int, default=10)
 CELERY_DEFAULT_MAX_RETRIES = config("CELERY_DEFAULT_MAX_RETRIES", cast=int, default=3)
